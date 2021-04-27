33b09629f0b33fc5cbb78df2739611b534ed4727
Prerna Jajodia <jajodiapj@gmail.com>
2021-04-27 10:23:44 +0530

    Footer added

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -2,15 +2,24 @@ import React from 'react';
 import { ToDos } from './ToDos';
 
 export const Footer = () => {
+  let footerStyle = {
+        position : "absolute",
+        width : "100%",
+        top : "100vh",
+        backgroundColor : "#ABB8C3"
+    }
 
     return (
 
-        <div>
+        <footer className = "bg-dark text-light py-3" style ={footerStyle}>
+            <p className = "text-center">
+                Copyright &copy; Prerna Jajodia
+            </p>
             <h4>
                  
             </h4>
 
 
-        </div>
+        </footer>
     )
 }
